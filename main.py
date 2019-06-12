b015471dc40c6ab28aadd688aa4dba1dd5336fc1
Ziyu <535220146@qq.com>
2019-06-12 11:05:09 +0800

    first push

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+import pandas as pd
+import sklearn
+import numpy as np
+from sklearn import tree
+
+if __name__ == "__main__":
+    print("Hello World")
+    #DataSet
+    #[size,weight,texture]
+    X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37],[166, 65, 40], [190, 90, 47], [175, 64, 39], [177, 70, 40], [159, 55, 37], [171, 75, 42], [181, 85, 43]]
+
+    Y = ['apple', 'apple', 'orange', 'orange', 'apple', 'apple', 'orange', 'orange','orange', 'apple', 'apple']
+
+    #classifier - DecisionTreeClassifier
+    clf_tree = tree.DecisionTreeClassifier()
+    clf_tree = clf_tree.fit(X,Y)
+
+    #test_data
+    test_data = [[190,70,42],[172,64,39],[182,80,42]]
+
+    #prediction
+    prediction_tree = clf_tree.predict(test_data)
+
+    print("Prediction of DecisionTreeClassifier:",prediction_tree)
+
